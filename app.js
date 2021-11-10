0ff9efba6531568b823bed0ca428948fe803ef20
Alex Williams <canablak@gmail.com>
2021-11-10 14:22:49 -0500

    firstCmsBuild

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const expressValidator = require('express-validator');
+//conect to db
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(config.database);
+  console.log('conected to mongoDB')
+}
+//init app
+const app = express();
+
+//View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Set global variable
+app.locals.err = null;
+
+//Body parser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+//express session middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true,
+  //cookie: { secure: true }
+}))
+
+//Express validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg,value){
+    const namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root; 
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg : msg,
+      value : value
+    };
+  }
+}));
+
+//Express messages middleware
+app.use(require('connect-flash')());
+app.use(function(req,res,next){
+  res.locals.messages = require('express-messages')(req,res);
+  next();
+});
+
+//Routes
+const pages = require('./routes/pages');
+const adminPages = require('./routes/admin_pages');
+const adminCategories = require('./routes/admin_categories');
+ 
+app.use('/admin/pages', adminPages);
+app.use('/admin/categories', adminCategories);
+app.use('/', pages);
+
+//Start server
+const port = 3000;
+app.listen(port, (err)=>{
+    if(err){
+        console.log(err);
+    }
+    console.log(`app listening port ` + port);
+})
